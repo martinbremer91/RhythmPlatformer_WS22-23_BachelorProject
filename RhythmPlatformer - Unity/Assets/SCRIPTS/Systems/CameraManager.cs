686c87f8bde69f15817d3cbb3c677d2469710178
martinbremer91 <63402364+martinbremer91@users.noreply.github.com>
2022-09-12 11:23:57 +0200

    fixed cam edges out of bounds error

--- RhythmPlatformer - Unity/Assets/SCRIPTS/Systems/CameraManager.cs
+++ RhythmPlatformer - Unity/Assets/SCRIPTS/Systems/CameraManager.cs
@@ -20,11 +20,11 @@ namespace Systems
         
         private readonly List<float> _nodeDistances = new();
 
-        private CameraBounds _center;
-        private CameraBounds _north;
-        private CameraBounds _west;
-        private CameraBounds _south;
-        private CameraBounds _east;
+        private CameraBounds _centerBounds;
+        private CameraBounds _northBounds;
+        private CameraBounds _westBounds;
+        private CameraBounds _southBounds;
+        private CameraBounds _eastBounds;
 
         private Vector2 _camSize;
 
@@ -61,11 +61,23 @@ namespace Systems
             _camSize.y = _cam.orthographicSize;
             _camSize.x = _camSize.y * _cam.aspect;
 
-            _center = GetCamBounds(position);
-            _north = GetCamBounds(new Vector3(position.x, position.y + _camSize.y, 0));
-            _west = GetCamBounds(new Vector3(position.x - _camSize.x, position.y, 0));
-            _south = GetCamBounds(new Vector3(position.x, position.y - _camSize.y, 0));
-            _east = GetCamBounds(new Vector3(position.x + _camSize.x, position.y, 0));
+            _centerBounds = GetCamBounds(position);
+            
+            Vector3 northPos = new Vector3(position.x, position.y + _camSize.y, 0);
+            if (CheckPointInBounds(northPos, _centerBounds))
+                _northBounds = GetCamBounds(northPos);
+            
+            Vector3 westPos = new Vector3(position.x - _camSize.x, position.y, 0);
+            if(CheckPointInBounds(westPos, _centerBounds))
+                _westBounds = GetCamBounds(westPos);
+            
+            Vector3 southPos = new Vector3(position.x, position.y - _camSize.y, 0);
+            if (CheckPointInBounds(southPos, _centerBounds))
+                _southBounds = GetCamBounds(southPos);
+            
+            Vector3 eastPos = new Vector3(position.x + _camSize.x, position.y, 0);
+            if(CheckPointInBounds(eastPos, _centerBounds))
+                _eastBounds = GetCamBounds(eastPos);
 
             CameraBounds GetCamBounds(Vector3 in_pos)
             {
@@ -77,8 +89,8 @@ namespace Systems
                     _nodeDistances[i] = Vector3.Distance(cn.Position, in_pos);
                 }
 
-                // TODO: these variables can probably be local, since the only thing that uses them out of scope
-                // TODO: is OnDrawGizmos
+                // TODO: these variables can probably be local, since the only
+                // TODO: thing that uses them out of scope is OnDrawGizmos
                 camBounds.CurrentNW =
                     GetClosestNode(_camNodes.Where(n => n.Position.x <= in_pos.x && n.Position.y >= in_pos.y));
                 camBounds.CurrentNE = 
@@ -88,7 +100,7 @@ namespace Systems
                 camBounds.CurrentSE = 
                     GetClosestNode(_camNodes.Where(n => n.Position.x >= in_pos.x && n.Position.y <= in_pos.y));
 
-                // TODO: only _center needs all these values. E.g. _west only needs MaxY and MinY
+                // TODO: only _centerBounds needs all these values. E.g. _westBounds only needs MaxY and MinY
                 camBounds.MaxY = Mathf.Max(camBounds.CurrentNW.y, camBounds.CurrentNE.y);
                 camBounds.MinY = Mathf.Min(camBounds.CurrentSW.y, camBounds.CurrentSE.y);
                 camBounds.MinX = Mathf.Min(camBounds.CurrentNW.x, camBounds.CurrentSW.x);
@@ -102,6 +114,12 @@ namespace Systems
                 return nodes.Aggregate((minD, n) => 
                         _nodeDistances[n.Index] < _nodeDistances[minD.Index] ? n : minD).Position;
             }
+
+            bool CheckPointInBounds(Vector3 in_point, CameraBounds in_bounds)
+            {
+                return in_point.x > in_bounds.MinX && in_point.x < in_bounds.MaxX && in_point.y > in_bounds.MinY &&
+                       in_point.y < in_bounds.MaxY;
+            }
         }
 
         private void LateUpdate()
@@ -112,15 +130,15 @@ namespace Systems
             Vector3 targetPos = _characterTf.position;
             Vector3 position = transform.position;
 
-            if (targetPos.x + _camSize.x > _center.MaxX)
-                targetPos.x = _center.MaxX - _camSize.x;
-            else if (targetPos.x - _camSize.x < _center.MinX)
-                targetPos.x = _center.MinX + _camSize.x;
+            if (targetPos.x + _camSize.x > _centerBounds.MaxX)
+                targetPos.x = _centerBounds.MaxX - _camSize.x;
+            else if (targetPos.x - _camSize.x < _centerBounds.MinX)
+                targetPos.x = _centerBounds.MinX + _camSize.x;
 
-            if (targetPos.y + _camSize.y > _center.MaxY)
-                targetPos.y = _center.MaxY - _camSize.y;
-            else if (targetPos.y - _camSize.y < _center.MinY)
-                targetPos.y = _center.MinY + _camSize.y;
+            if (targetPos.y + _camSize.y > _centerBounds.MaxY)
+                targetPos.y = _centerBounds.MaxY - _camSize.y;
+            else if (targetPos.y - _camSize.y < _centerBounds.MinY)
+                targetPos.y = _centerBounds.MinY + _camSize.y;
             
             position = 
                 Vector3.SmoothDamp(position, new Vector3(targetPos.x, targetPos.y, position.z), 
@@ -128,12 +146,13 @@ namespace Systems
             
             transform.position = position;
 
-            float minVertSize = Mathf.Min(_west.MaxY - _west.MinY, _east.MaxY - _east.MinY) * .5f;
+            float minVertSize = Mathf.Min(_westBounds.MaxY - _westBounds.MinY, _eastBounds.MaxY - _eastBounds.MinY) * .5f;
             
-            float minHorSize = Mathf.Min(_north.MaxX - _north.MinX, _south.MaxX - _south.MinX) * .5f;
+            float minHorSize = Mathf.Min(_northBounds.MaxX - _northBounds.MinX, _southBounds.MaxX - _southBounds.MinX) * .5f;
             float vertComplement = minHorSize / _cam.aspect;
 
-            float targetSize = Mathf.Min(minVertSize, vertComplement) - .5f;
+            float targetSize = Mathf.Min(minVertSize, vertComplement) - .05f;
+            
             _cam.orthographicSize = Mathf.Lerp(_cam.orthographicSize, targetSize, Time.deltaTime * 2);
         }
 
@@ -163,33 +182,33 @@ namespace Systems
             
             Gizmos.color = Color.blue;
             
-            Gizmos.DrawWireSphere(_center.CurrentNW, .5f);
-            Gizmos.DrawWireSphere(_center.CurrentNE, .5f);
-            Gizmos.DrawWireSphere(_center.CurrentSW, .5f);
-            Gizmos.DrawWireSphere(_center.CurrentSE, .5f);
+            Gizmos.DrawWireSphere(_centerBounds.CurrentNW, .5f);
+            Gizmos.DrawWireSphere(_centerBounds.CurrentNE, .5f);
+            Gizmos.DrawWireSphere(_centerBounds.CurrentSW, .5f);
+            Gizmos.DrawWireSphere(_centerBounds.CurrentSE, .5f);
             
             Gizmos.color = Color.magenta;
             
-            Gizmos.DrawWireSphere(new Vector3(pos.x, _center.MaxY, 0), .5f);
-            Gizmos.DrawWireSphere(new Vector3(pos.x, _center.MinY, 0), .5f);
-            Gizmos.DrawWireSphere(new Vector3(_center.MinX, pos.y, 0), .5f);
-            Gizmos.DrawWireSphere(new Vector3(_center.MaxX, pos.y, 0), .5f);
+            Gizmos.DrawWireSphere(new Vector3(pos.x, _centerBounds.MaxY, 0), .5f);
+            Gizmos.DrawWireSphere(new Vector3(pos.x, _centerBounds.MinY, 0), .5f);
+            Gizmos.DrawWireSphere(new Vector3(_centerBounds.MinX, pos.y, 0), .5f);
+            Gizmos.DrawWireSphere(new Vector3(_centerBounds.MaxX, pos.y, 0), .5f);
             
             Gizmos.color = Color.cyan;
-            Gizmos.DrawWireSphere(new Vector3(_north.MinX, position.y + _camSize.y, 0), .3f);
-            Gizmos.DrawWireSphere(new Vector3(_north.MaxX, position.y + _camSize.y, 0), .3f);
+            Gizmos.DrawWireSphere(new Vector3(_northBounds.MinX, position.y + _camSize.y, 0), .3f);
+            Gizmos.DrawWireSphere(new Vector3(_northBounds.MaxX, position.y + _camSize.y, 0), .3f);
             
             Gizmos.color = Color.green;
-            Gizmos.DrawWireSphere(new Vector3(position.x - _camSize.x, _west.MinY, 0), .3f);
-            Gizmos.DrawWireSphere(new Vector3(position.x - _camSize.x, _west.MaxY, 0), .3f);
+            Gizmos.DrawWireSphere(new Vector3(position.x - _camSize.x, _westBounds.MinY, 0), .3f);
+            Gizmos.DrawWireSphere(new Vector3(position.x - _camSize.x, _westBounds.MaxY, 0), .3f);
             
             Gizmos.color = Color.yellow;
-            Gizmos.DrawWireSphere(new Vector3(_south.MinX, position.y - _camSize.y, 0), .3f);
-            Gizmos.DrawWireSphere(new Vector3(_south.MaxX, position.y - _camSize.y, 0), .3f);
+            Gizmos.DrawWireSphere(new Vector3(_southBounds.MinX, position.y - _camSize.y, 0), .3f);
+            Gizmos.DrawWireSphere(new Vector3(_southBounds.MaxX, position.y - _camSize.y, 0), .3f);
             
             Gizmos.color = Color.white;
-            Gizmos.DrawWireSphere(new Vector3(position.x + _camSize.x, _east.MinY, 0), .3f);
-            Gizmos.DrawWireSphere(new Vector3(position.x + _camSize.x, _east.MaxY, 0), .3f);
+            Gizmos.DrawWireSphere(new Vector3(position.x + _camSize.x, _eastBounds.MinY, 0), .3f);
+            Gizmos.DrawWireSphere(new Vector3(position.x + _camSize.x, _eastBounds.MaxY, 0), .3f);
         }
 #endif
     }
