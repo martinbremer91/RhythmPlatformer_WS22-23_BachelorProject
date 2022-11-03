0141c33138f59a8d7079dd827d3d3691e2d687a5
martinbremer91 <63402364+martinbremer91@users.noreply.github.com>
2022-11-03 16:06:09 +0100

    Added ping Scenes folder function to Scenes menu

--- RhythmPlatformer - Unity/Assets/Editor/SceneLoaderMenu.cs
+++ RhythmPlatformer - Unity/Assets/Editor/SceneLoaderMenu.cs
@@ -6,6 +6,10 @@ namespace Editor
 {
     public static class SceneLoaderMenu
     {
+        [MenuItem("Scenes/SCENES FOLDER")]
+        private static void SelectScenesFolder() =>
+            EditorGUIUtility.PingObject(AssetDatabase.LoadAssetAtPath("Assets/Scenes", typeof(UnityEngine.Object)));
+
         [MenuItem("Scenes/Main Menu")]
         private static void LoadMainMenu() =>
             EditorSceneManager.OpenScene($"Assets/Scenes/{Constants.MainMenu}.unity");
