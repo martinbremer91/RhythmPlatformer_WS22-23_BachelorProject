3deb7c3bdf6d78de3d90b463e76d8fe70836c83b
martinbremer91 <63402364+martinbremer91@users.noreply.github.com>
2022-11-10 19:00:11 +0100

    crouching now lets player fall through platform

--- RhythmPlatformer - Unity/Assets/SCRIPTS/GameplaySystems/MovingPlatform.cs
+++ RhythmPlatformer - Unity/Assets/SCRIPTS/GameplaySystems/MovingPlatform.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Gameplay;
 using GlobalSystems;
 using Interfaces_and_Enums;
@@ -8,15 +9,21 @@ namespace GameplaySystems
     public class MovingPlatform : MonoBehaviour, IInit<GameStateManager, MovementRoutine>
     {
         private CharacterCollisionDetector _characterCollisionDetector;
+        private CharacterStateController _characterStateController;
         private MovementRoutine _movementRoutine;
+
+        [SerializeField] private BoxCollider2D _collider;
 
         private bool _isOneWayPlatform;
         
         public void Init(GameStateManager in_gameStateManager, MovementRoutine in_movementRoutine)
         {
+            _isOneWayPlatform = gameObject.CompareTag("OneWayPlatform");
+            if (_isOneWayPlatform)
+                _characterStateController = in_gameStateManager.CharacterStateController;
+            
             _characterCollisionDetector = in_gameStateManager.CharacterCollisionDetector;
             _movementRoutine = in_movementRoutine;
-            _isOneWayPlatform = gameObject.CompareTag("OneWayPlatform");
         }
         
         private void OnCollisionEnter2D(Collision2D col)
@@ -29,6 +36,15 @@ namespace GameplaySystems
             }
         }
 
+        private void OnCollisionStay2D(Collision2D collision)
+        {
+            if (!_isOneWayPlatform)
+                return;
+
+            if (_characterStateController.CurrentCharacterState == CharacterState.Crouch)
+                ExecuteFallThrough();
+        }
+
         private void OnCollisionExit2D(Collision2D other)
         {
             if (other.enabled && other.gameObject.CompareTag("Player"))
@@ -37,6 +53,16 @@ namespace GameplaySystems
                     _characterCollisionDetector.OnOneWayPlatform = false;
                 _movementRoutine.MovePlayerAsWell = false;
             }
+        }
+
+        private async void ExecuteFallThrough()
+        {
+            _characterCollisionDetector.OnOneWayPlatform = false;
+            _movementRoutine.MovePlayerAsWell = false;
+
+            _collider.enabled = false;
+            await Task.Delay(500);
+            _collider.enabled = true;
         }
     }
 }
