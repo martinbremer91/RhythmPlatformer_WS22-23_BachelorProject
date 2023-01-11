ffd0bfd5e789829b35e130c37409c1f8e1f312db
martinbremer91 <martin.bremer91@gmail.com>
2023-01-11 14:12:04 +0100

    added dash crystal burst particles

--- RhythmPlatformer - Unity/Assets/SCRIPTS/GameplaySystems/DashCrystal.cs
+++ RhythmPlatformer - Unity/Assets/SCRIPTS/GameplaySystems/DashCrystal.cs
@@ -4,11 +4,14 @@ namespace GameplaySystems
 {
     public class DashCrystal : CrystalBase
     {
+        [SerializeField] private ParticleSystem _burstParticleSystem;
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            if (!collision.gameObject.CompareTag("Player") || _characterStateController.Dead)
+            if (!collision.gameObject.CompareTag("Player") || _characterStateController.CanDash || _characterStateController.Dead)
                 return;
 
+            _burstParticleSystem.Play();
             _characterStateController.CanDash = true;
         }
     }
