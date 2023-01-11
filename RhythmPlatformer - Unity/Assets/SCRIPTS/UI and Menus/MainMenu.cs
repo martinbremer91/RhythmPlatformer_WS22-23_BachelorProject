1560b9d219f0ea6d726c8605cda3b71ffb857727
martinbremer91 <martin.bremer91@gmail.com>
2023-01-11 20:39:41 +0100

    new game button now starts first element in level sequence array

--- RhythmPlatformer - Unity/Assets/SCRIPTS/UI and Menus/MainMenu.cs
+++ RhythmPlatformer - Unity/Assets/SCRIPTS/UI and Menus/MainMenu.cs
@@ -1,15 +1,27 @@
 using GlobalSystems;
 using UnityEngine;
+using Scriptable_Object_Scripts;
 using Utility_Scripts;
+using System.Linq;
 
 namespace UI_And_Menus
 {
     public class MainMenu : MonoBehaviour
     {
-        [SerializeField] UiManager _uiManager;
+        [SerializeField] private UiManager _uiManager;
+        [SerializeField] private LevelSequenceData _levelSequenceData;
 
         public void LoadLevel() => 
-            StartCoroutine(SceneLoadManager.LoadSceneCoroutine(Constants.DemoLevel, _uiManager));
+            StartCoroutine(SceneLoadManager.LoadSceneCoroutine(GetFirstLevelName(), _uiManager));
         public void CloseGame() => Application.Quit();
+
+        private string GetFirstLevelName()
+        {
+            if (_levelSequenceData == null || _levelSequenceData.LevelSequence == null || 
+                !_levelSequenceData.LevelSequence.Any())
+                return Constants.DemoLevel;
+
+            return _levelSequenceData.LevelSequence[0];
+        }
     }
 }
